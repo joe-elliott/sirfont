2c248dba25e15b580dcf8a637ab26ced8f2b2004
unknown <Joe Elliott>
2016-06-18 22:56:40 -0400

    Moved AppWindow into components

--- app/index.js
+++ app/index.js
@@ -1,19 +1,7 @@
-var react = require('../node_modules/react');
-var render = require('../node_modules/react-dom');
+var react = require('react');
+var render = require('react-dom');
 
-var AppWindow = react.createClass({
-    displayName: 'appWindow',
-    render: function() {
-        return react.createElement(
-            'div',
-            null,
-            react.createElement('div', { className: 'master-pane'}),
-            react.createElement('div', { className: 'detail-pane'}, 
-                react.createElement('textarea')
-            )
-        );
-    }
-});
+var AppWindow = require('../components/AppWindow.js');
 
 render.render(
     react.createElement(AppWindow, null),
